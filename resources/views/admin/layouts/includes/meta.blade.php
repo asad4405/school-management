d3019db6bf09310d22c25094405f615c714a5c02
Asad <asadrifat450@gmail.com>
2026-02-02 23:16:37 +0600

    changes index.php , .htaccess and backend admin panel design added

---
+++ resources/views/admin/layouts/includes/meta.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Dashboard</title>
+
+    <!-- Site favicon -->
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="{{ asset('public/admin/assets/vendors') }}/images/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="{{ asset('public/admin/assets/vendors') }}/images/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="{{ asset('public/admin/assets/vendors') }}/images/favicon-16x16.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/admin/assets/vendors') }}/styles/core.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/admin/assets/vendors') }}/styles/icon-font.min.css" />
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('public/admin/assets/src') }}/plugins/datatables/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('public/admin/assets/src') }}/plugins/datatables/css/responsive.bootstrap4.min.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/admin/assets/vendors') }}/styles/style.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+</head>
+
+<body>
---
+++ resources/views/admin/layouts/includes/script.blade.php
@@ -0,0 +1,14 @@
+<!-- js -->
+<script src="{{ asset('public/admin/assets/vendors') }}/scripts/core.js"></script>
+<script src="{{ asset('public/admin/assets/vendors') }}/scripts/script.min.js"></script>
+<script src="{{ asset('public/admin/assets/vendors') }}/scripts/process.js"></script>
+<script src="{{ asset('public/admin/assets/vendors') }}/scripts/layout-settings.js"></script>
+<script src="{{ asset('public/admin/assets/src') }}/plugins/apexcharts/apexcharts.min.js"></script>
+<script src="{{ asset('public/admin/assets/src') }}/plugins/datatables/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('public/admin/assets/src') }}/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('public/admin/assets/src') }}/plugins/datatables/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('public/admin/assets/src') }}/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('public/admin/assets/vendors') }}/scripts/dashboard3.js"></script>
+</body>
+
+</html>
