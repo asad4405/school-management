f9645cb836f43350e17d2c0be815ea34cd76b75b
Asad <asadrifat450@gmail.com>
2026-02-03 22:14:28 +0600

    login page , logout,auth middleware

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => '/admin'], function () {
+Route::group(['middleware' => 'auth','prefix' => '/admin'], function () {
     // admin dashboard
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
 });
