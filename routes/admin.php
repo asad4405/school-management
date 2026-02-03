1513c38fcf93285e431ed22814f60d33b9d2c655
Asad <asadrifat450@gmail.com>
2026-02-03 22:46:01 +0600

    create admin controller and show admin list

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth','prefix' => '/admin'], function () {
     // admin dashboard
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
+
+    // admin list
+    Route::get('/list',[AdminController::class,'admin_list'])->name('admin.list');
 });
